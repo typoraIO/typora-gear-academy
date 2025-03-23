60fa49f5583c6792bb47078e28685d6ede971169
typoraIO <typora.io@hotmail.com>
2025-03-23 21:51:45 +0800

    Add files via upload

---
+++ pebble-game/src/lib.rs
@@ -0,0 +1,23 @@
+#![no_std]
+#![allow(warnings)]
+
+use gstd::*;
+use pebble_game_io::*;
+
+
+static mut PEBBLE_GAME: Option<GameState> = None;
+
+#[no_mangle]
+extern "C" fn init() {
+
+}
+
+#[no_mangle]
+extern "C" fn handle() {
+
+}
+
+#[no_mangle]
+extern "C" fn state() {
+
+}
